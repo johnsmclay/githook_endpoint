26a7a1f7408c95378405488417c572b5294693aa
Clay Johns <clay.johns@moneydesktop.com>
2012-06-13 12:12:01 -0600

    removed puppet from repo

--- hooks/puppet.sh
+++
@@ -1,7 +0,0 @@
-ssh jamhead@172.16.11.192 -o StrictHostKeyChecking=no "cd /etc/puppet ; sudo git reset --hard ; sudo git checkout master ; sudo git pull origin master"
-RETVAL=$?
-if [ $RETVAL -ne 0 ]; then echo "failed to deploy to puppetmaster"; else echo "master was deployed if changes exist"; fi
-
-ssh jamhead@172.16.11.193 -o StrictHostKeyChecking=no "cd /etc/puppet ; sudo git reset --hard ; sudo git pull origin stages ; sudo git checkout stages"
-RETVAL=$?
-if [ $RETVAL -ne 0 ]; then echo "failed to deploy to puppetmaster2"; else echo "stages was deployed if changes exist"; fi
